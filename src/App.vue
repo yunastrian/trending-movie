2ccadf3ee2a230d65d73e993697346f85bea16bd
Kurniandha Sukma Yunastrian <13516106@std.stei.itb.ac.id>
2020-08-09 14:08:05 +0700

    add trending movie

--- src/App.vue
+++ src/App.vue
@@ -6,55 +6,95 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h2>Trending Movie</h2>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <div class="d-flex align-center">
+        <h4>{{ date }}</h4>
+      </div>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-item-group>
+        <v-container>
+          <v-row>
+            <v-col
+              v-for="movie in movies"
+              :key="movie.id"
+              cols="12"
+              md="4"
+            >
+              <v-card
+                class="mx-auto"
+                max-width="400"
+              >
+                <v-img
+                  class="white--text align-end"
+                  height="200px"
+                  :src="`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`"
+                >
+                </v-img>
+
+                <v-card-title>{{ movie.title }} ({{ movie.release_date.substring(0, 4) }})</v-card-title>
+                <v-card-subtitle class="pb-0">score: {{ movie.vote_average }} / 10</v-card-subtitle>
+                
+                <v-card-text class="text--primary">
+                  <div>{{ movie.overview }}</div>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-btn
+                    color="primary"
+                    text
+                    :href="`https://www.themoviedb.org/movie/${movie.id}`"
+                  >
+                    Detail
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-item-group>
     </v-main>
+
+    <v-footer 
+      app
+      color="primary"
+      dark
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <footer-custom year="2020" author="Kurniandha Sukma Yunastrian"></footer-custom>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import axios from 'axios'
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    date: null,
+    movies: null
   }),
+  beforeMount() {
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var d = new Date();
+    
+    this.date = `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
+    axios
+      .get('https://api.themoviedb.org/3/trending/movie/day?api_key=81b50f79b05566f0f3721736ae5ab573')
+      .then(response => {
+        this.movies = response.data.results
+      });
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+class FooterCustom extends HTMLElement {
+  constructor() {
+    super();
+  }
+  
+  
+  connectedCallback() {
+    this.year = this.getAttribute("year") || null;
+    this.author = this.getAttribute("author") || null;
+
+    this.innerHTML = `&copy; ${this.year} ${this.author}`;
+  }
+}
+customElements.define('footer-custom', FooterCustom);
 
 new Vue({
   vuetify,
